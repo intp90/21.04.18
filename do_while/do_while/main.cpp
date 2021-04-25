6a22f9139276a5d35018e3aac3083d1b0e44084f
intp90 <intp90@naver.com>
2021-04-25 17:01:17 +0900

    do_while

---
+++ do_while/do_while/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int Input;
+
+	do
+	{
+		std::cout << "1" << std::endl;
+		std::cout << "2" << std::endl;
+		std::cout << "3" << std::endl;
+		std::cin >> Input;
+	} while (Input <= 0 || Input >= 3);
+
+	std::cout << "ok" << std::endl;
+
+	return 0;
+
+}
+
+
